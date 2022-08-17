ddfe014042478e5584b39baedf3429fa2cf41570
saksham345 <kumarsaksham601@gmail.com>
2022-08-17 23:27:10 +0530

    added layouts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayouts);
 
 app.use('/', require('./routes/index'));
 
