be21d77943353f8216b619bba665479ee63bb951
saksham345 <kumarsaksham601@gmail.com>
2022-08-17 23:36:55 +0530

    added static files

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const expressLayouts = require('express-ejs-layouts');
 
 app.use(express.static('./assets'));
 app.use(expressLayouts);
+//extract styles and script from layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes/index'));
 
