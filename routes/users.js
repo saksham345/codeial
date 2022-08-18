e97dab2cab3fecb3c0cf81c939d7e9825a58b0a6
saksham345 <kumarsaksham601@gmail.com>
2022-08-18 21:06:41 +0530

    created form for user sign up and sign in

--- routes/users.js
+++ routes/users.js
@@ -3,5 +3,9 @@ const router = express.Router();
 
 const usersController = require('../controllers/user_controller');
 
-router.get('/profile', usersController.profile) 
+router.get('/profile', usersController.profile);
+
+router.get('/sign-up', usersController.signUp);
+router.get('/sign-in',usersController.signIn);
+
 module.exports = router;
