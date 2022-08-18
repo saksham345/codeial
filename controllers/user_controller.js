d9bf7bc5ac6541436b16a2157ec66ce129e63e8d
saksham345 <kumarsaksham601@gmail.com>
2022-08-18 20:26:15 +0530

    Added Sign in and sign up

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,19 @@
+const { model } = require("mongoose");
+
 module.exports.profile = function(req, res){
     return res.render('./user_profile.ejs',{
         title: "Profile"
     });
+}
+
+module.exports.signUp = function(req, res){
+    return res.render('user_sign_up', {
+    title: "Ceodial || Sign Up"
+    })
+}
+
+module.exports.signIn = function(req, res){
+    return res.render('user_sign_in',{
+        title: "Codeial || User Sign In"
+    })
 }
