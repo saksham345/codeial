e9849546c5713d7ae23483332ae7f8156b267206
saksham345 <kumarsaksham601@gmail.com>
2022-08-29 20:17:23 +0530

    display posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,21 @@
+const Post = require("../models/post")
+
 module.exports.home = function(req, res){
-    return res.render('./home.ejs',{
-        title: "Home"
-    });
+    // return res.render('./home.ejs',{
+    //     title: "Home"
+    // });
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home',{
+    //         title: 'Home', 
+    //         posts: posts
+    //     });
+    // });
+
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home',{
+                    title: 'Home', 
+                    posts: posts
+                });
+    })
 }
