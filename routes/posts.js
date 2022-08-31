ef02acc51aa4a5b9dff20a1064379698bf4d92ef
saksham345 <kumarsaksham601@gmail.com>
2022-08-31 20:34:05 +0530

    added delete post feature

--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const PostController = require('../controllers/post_controllers');
 
 router.post('/create', passport.checkAuthentication, PostController.create);
+router.get('/destroy/:id', passport.checkAuthentication, PostController.destroy);
 
 module.exports = router;
