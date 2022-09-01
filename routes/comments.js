3e22a0084634fcdd246aaff32e2849a5e5533329
saksham345 <kumarsaksham601@gmail.com>
2022-09-01 14:15:51 +0530

    added delete comment option

--- routes/comments.js
+++ routes/comments.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const commentsController = require('../controllers/comments_contoller');
 
 router.post('/create',passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
