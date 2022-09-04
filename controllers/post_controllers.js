004b9b1d4fa0f6e693e8521cacf20dec895ce2f5
saksham345 <kumarsaksham601@gmail.com>
2022-09-05 00:14:44 +0530

    added various flashes

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -7,26 +7,31 @@ module.exports.create = async function(req, res){
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success', 'Post Published!');
     
         return res.redirect('back');
 
     }
     catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 }
 
 module.exports.destroy = async function(req, res){
     try{
-    let posts = await Post.findById(req.params.id);
+    let post = await Post.findById(req.params.id);
     //.id == converting ._id to string
     if(post.user == req.user.id){
         post.remove();
 
         await Comment.deleteMany({post: req.params.id});
+
+        req.flash('success', 'Post Deleted!');
         return res.redirect('back');
     }
     else{
+        req.flash('error', 'Cannot Delete this post');
         return res.redirect('back');
     }
     }
--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -61,12 +61,14 @@ module.exports.create = function(req, res){
 }
 
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in Successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout(function(err) {
         if (err) { console.log('Error in signing out'); /*return next(err);*/ }
+        req.flash('success', 'Logged Out  Successfully');
         res.redirect('/');
     }
     )};
